fc336a7b3a2d02102a554d8c2e217a7edd924215
Jimmy Jiang <james.jiang30@gmail.com>
2021-07-06 14:05:46 -0600

    Added check for if there are no spelled items available

--- main.py
+++ main.py
@@ -3,13 +3,15 @@ import re
 import json
 from bs4 import BeautifulSoup
 import pandas as pd
+from pprint import pprint
 
 
 def generate_spelled_listing_data(item_data_script):
-    returned_dictionary = re.findall("g_rgAssets = .*;", str(item_data_script))[0]
-    returned_dictionary = re.findall("{.*};", str(returned_dictionary))[0]
+    returned_dictionary = re.findall("g_rgAssets = .*;", str(item_data_script))[0]  # find g_rgAssets object
+    returned_dictionary = re.findall("{.*};", str(returned_dictionary))[0]  # get the g_rgAssets object itself
     returned_dictionary = returned_dictionary[0: -1]   # get rid of semicolon at end
     returned_dictionary = json.loads(returned_dictionary)
+    pprint(returned_dictionary)
     return returned_dictionary
 
 
@@ -83,36 +85,38 @@ def main():
         listings_table = soup.find(id="searchResultsRows")
         spelled_listing_descriptions = soup.find_all(class_="item_desc_description")
 
-        spelled_listing_rows = listings_table.find_all(class_="market_listing_row")
-        spelled_listing_prices = listings_table.find_all(class_="market_listing_price_with_fee")
-        spelled_listing_names = listings_table.find_all(class_="market_listing_item_name")
+        if listings_table is not None:
+            spelled_listing_rows = listings_table.find_all(class_="market_listing_row")
+            spelled_listing_prices = listings_table.find_all(class_="market_listing_price_with_fee")
+            spelled_listing_names = listings_table.find_all(class_="market_listing_item_name")
 
-        # the "g_rgAssets" variable is responsible for describing the market listings
-        item_data_script = ""
-        spelled_listing_data = {}
-        all_webpage_javascript = soup.find_all(type="text/javascript")  # array of JS scripts in webpage
+            # the "g_rgAssets" variable is responsible for describing the market listings
+            item_data_script = ""
+            spelled_listing_data = {}
+            all_webpage_javascript = soup.find_all(type="text/javascript")  # array of JS scripts in webpage
 
-        for script in all_webpage_javascript:
-            if (not ("src" in script.attrs)) and ("g_rgAssets" in str(script)):
-                item_data_script = script
+            for script in all_webpage_javascript:
+                if (not ("src" in script.attrs)) and ("g_rgAssets" in str(script)):
+                    item_data_script = script
+                    break
 
-        spelled_listing_data = generate_spelled_listing_data(item_data_script)
+            spelled_listing_data = generate_spelled_listing_data(item_data_script)
 
-        # list determining which spelled items are valid
-        spelled_items_validity = get_valid_spelled_items(spelled_listing_data)
+            # list determining which spelled items are valid
+            spelled_items_validity = get_valid_spelled_items(spelled_listing_data)
 
-        all_spelled_items_names.extend(spelled_listing_names)
-        all_spelled_items_prices.extend(spelled_listing_prices)
-        all_spelled_items_validity.extend(spelled_items_validity)
+            all_spelled_items_names.extend(spelled_listing_names)
+            all_spelled_items_prices.extend(spelled_listing_prices)
+            all_spelled_items_validity.extend(spelled_items_validity)
 
-        # for spelled_listing in spelled_listing_rows:
-        #     print(spelled_listing.prettify())
-        # for spelled_listing_price in spelled_listing_prices:
-        #     print(spelled_listing_price.text.strip())
-        # for spelled_listing_name in spelled_listing_names:
-        #     print(spelled_listing_name.text)
-        # for item_validity in spelled_items_validity:
-        #     print(item_validity)
+            # for spelled_listing in spelled_listing_rows:
+            #     print(spelled_listing.prettify())
+            # for spelled_listing_price in spelled_listing_prices:
+            #     print(spelled_listing_price.text.strip())
+            # for spelled_listing_name in spelled_listing_names:
+            #     print(spelled_listing_name.text)
+            # for item_validity in spelled_items_validity:
+            #     print(item_validity)
 
     df = generate_spelled_items_dataframe(all_spelled_items_prices, all_spelled_items_names, all_spelled_items_validity)
     print(df)
